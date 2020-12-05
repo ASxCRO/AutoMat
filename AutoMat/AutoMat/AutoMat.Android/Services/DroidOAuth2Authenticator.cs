8975da6521df47b253da680a4644898bf76f43ec
asxcro <antonio.suups@gmail.com>
2020-12-05 12:26:42 +0100

    oauth models

---
+++ AutoMat/AutoMat/AutoMat.Android/Services/DroidOAuth2Authenticator.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Auth;
+
+namespace AutoMat.Droid.Services
+{
+    public class DroidOAuth2Authenticator : OAuth2Authenticator
+    {
+        public DroidOAuth2Authenticator(string clientId, string scope, Uri authorizeUrl, Uri redirectUrl) : base(clientId, scope, authorizeUrl, redirectUrl)
+        {
+
+        }
+
+        protected override void OnPageEncountered(Uri url, System.Collections.Generic.IDictionary<string, string> query, System.Collections.Generic.IDictionary<string, string> fragment)
+        {
+            // Remove state from dictionaries. 
+            // We are ignoring request state forgery status 
+            // as we're hitting an ASP.NET service which forwards 
+            // to a third-party OAuth service itself
+            if (query.ContainsKey("state"))
+            {
+                query.Remove("state");
+            }
+
+            if (fragment.ContainsKey("state"))
+            {
+                fragment.Remove("state");
+            }
+
+            base.OnPageEncountered(url, query, fragment);
+        }
+    }
+}
---
+++ AutoMat/AutoMat/AutoMat/Core/ViewModels/FirebaseUser.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoMat.Core.ViewModels
+{
+    public class FirebaseUser
+    {
+        public string Email { get; set; }
+        public string Prezime { get; set; }
+        public string Ime { get; set; }
+    }
+}
